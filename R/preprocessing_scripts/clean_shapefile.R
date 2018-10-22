511297137c36c06637d10c1555fa7aa36b4b704e
Katie Jolly <katiejolly6@gmail.com>
2018-10-21 22:58:21 -0500

    update preprocessing

--- R/preprocessing_scripts/clean_shapefile.R
+++ R/preprocessing_scripts/clean_shapefile.R
@@ -20,5 +20,20 @@ sum(st_is_valid(precincts_valid) == FALSE)
 precincts_collection <- precincts_valid %>%
   st_cast("GEOMETRYCOLLECTION")
 
-st_write(precincts_collection, "shp/precincts_results_val.gpkg")
+st_write(obj = precincts_collection, dsn = "shp/precincts_results_val.gpkg")
 
+ggplot(precincts_collection) +
+  geom_sf()
+
+collections <- precincts_valid %>% filter(st_geometry_type(.) == "GEOMETRYCOLLECTION")
+
+gpkg_test <- st_read("shp/precincts_results_val.gpkg")
+
+st_write(obj = precincts_no_collections, "shp/precincts_multipolygon.gpkg")
+
+
+ggplot(gpkg_test %>% rename(geometry = geom)) +
+  geom_sf()
+
+precincts_no_collections <- anti_join(precincts_valid, collections %>% st_set_geometry(NULL)) %>%
+  st_cast("MULTIPOLYGON")
